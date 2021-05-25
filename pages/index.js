e6a4c0fc12c234c07549bf26256c03785e15b2ca
Galen Lewis <galenlewis@Galens-Air.fios-router.home>
2021-05-25 18:32:04 -0400

    changed to not welcome

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Welcome to my personal site! I plan to post regularly about startups, technology, and UX design.</p>
+        <p>Not Welcome to my personal site! I plan to post regularly about startups, technology, and UX design.</p>
         <p>A bit about me: I'm a Co-Founder at Mark Labs (venture-backed software company in the ESG space) and a Venture Fellow at Glynn Capital (VC in Silicon Valley). I've worked across Product, Sales, and Marketing at early and growth-stage startups including Posh Technologies, a conversational AI fintech. I'm currently an undergraduate student at Harvard University where I'm studying Computer Science and Psychology.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
