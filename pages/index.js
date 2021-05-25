1feacc310ad2f1ce1d948a28bb05c69f0514d98b
Galen Lewis <galenlewis@Galens-Air.fios-router.home>
2021-05-25 04:20:42 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Welcome to my personal site! I plan to post regularly about startups, technology, and UX design.</p>
+        <p>A bit about me: I'm a Co-Founder at Mark Labs (venture-backed software company in the ESG space) and a Venture Fellow at Glynn Capital (VC in Silicon Valley). I've worked across Product, Sales, and Marketing at early and growth-stage startups including Posh Technologies, a conversational AI fintech. I'm currently an undergraduate student at Harvard University where I'm studying Computer Science and Psychology.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
